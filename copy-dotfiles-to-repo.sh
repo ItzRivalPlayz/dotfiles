66060df34c698ee6bec84e4ed1fc276fb64b3cb8
ItzRivalPlayz <93585425+ItzRivalPlayz@users.noreply.github.com>
2022-02-01 16:08:06 +0000

    Updated

--- copy-dotfiles-to-repo.sh
+++ copy-dotfiles-to-repo.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-cp $HOME/.xmonad /home/peter/GitHub-Repos/dotfiles/ --force -rv
-cp $HOME/.config/kitty /home/peter/GitHub-Repos/dotfiles/.config --force -rv
-cp $HOME/.config/xmobar /home/peter/GitHub-Repos/dotfiles/.config --force -r
-cp $HOME/.config/polybar /home/peter/GitHub-Repos/dotfiles/.config --force -rv
-cp $HOME/.config/bspwm /home/peter/GitHub-Repos/dotfiles/.config --force -rv
-cp $HOME/.config/sxhkd /home/peter/GitHub-Repos/dotfiles/.config --force -rv
-cp $HOME/.config/picom /home/peter/GitHub-Repos/dotfiles/.config --force -rv
-cp $HOME/.config/i3 /home/peter/GitHub-Repos/dotfiles/.config --force -rv
-cp $HOME/.bashrc /home/peter/GitHub-Repos/dotfiles/ --force -rv
-cp $HOME/.config/qtile /home/peter/GitHub-Repos/dotfiles/.config --force -rv
-cp $HOME/.config/starship.toml /home/peter/GitHub-Repos/dotfiles/.config --force -rv
+cp /home/peter/.xmonad /home/peter/Github-Repos/dotfiles/ --force -rv
+cp /home/peter/.config/kitty /home/peter/Github-Repos/dotfiles/.config --force -rv
+cp /home/peter/.config/xmobar /home/peter/Github-Repos/dotfiles/.config --force -r
+cp /home/peter/.config/polybar /home/peter/Github-Repos/dotfiles/.config --force -rv
+cp /home/peter/.config/bspwm /home/peter/Github-Repos/dotfiles/.config --force -rv
+cp /home/peter/.config/sxhkd /home/peter/Github-Repos/dotfiles/.config --force -rv
+cp /home/peter/.config/picom /home/peter/Github-Repos/dotfiles/.config --force -rv
+cp /home/peter/.config/i3 /home/peter/Github-Repos/dotfiles/.config --force -rv
+cp /home/peter/.bashrc /home/peter/Github-Repos/dotfiles/ --force -rv
+cp /home/peter/.config/qtile /home/peter/Github-Repos/dotfiles/.config --force -rv
+cp /home/peter/.config/starship.toml /home/peter/Github-Repos/dotfiles/.config --force -rv
