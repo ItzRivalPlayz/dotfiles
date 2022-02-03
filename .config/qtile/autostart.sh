79e9c06d48533a12a1069acce0f3440eb75a393a
ItzRivalPlayz <93585425+ItzRivalPlayz@users.noreply.github.com>
2022-02-03 13:13:47 +0000

    Updated
    => QTILE: AUTOSTART.SH

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -3,6 +3,6 @@
 lxsession &
 picom &
 nm-applet &
-nitrogen --set-zoom-fill --random $HOME/Pictures/wallpapers/ &
---output eDP-1-1 --mode 1920x1080 --pos 3840x0 --rotate normal &
+nitrogen --set-zoom-fill $HOME/Pictures/wallpapers/3.jpg &
+xrandr --output eDP-1-1 --mode 1920x1080 --pos 3840x0 --rotate normal &
 xrandr --output HDMI-0 --primary --mode 3840x1080 --pos 0x0 --rotate normal &
